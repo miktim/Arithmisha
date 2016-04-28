545faa1f104bb1e9df5e327a8ff5b62ad0f1d5d9
miktim <miktim@mail.ru>
2016-04-28 20:38:15 +0300

    First commit

---
+++ gitpush.bat
@@ -0,0 +1,3 @@
+rem git init
+rem git remote add origin https://github.com/miktim/Arithmisha.git
+git push  origin master
---
+++ lang_ru.js
@@ -0,0 +1,35 @@
+document.title = "Арифмиша";
+document.getElementById("areaMap").title = "От автора...";
+document.getElementById("areaTeacher").title = "Стро-о-огая Марьванна. Усложнить задание?";
+document.getElementById("areaMisha").title = "Я - Миша. Показать дневник?";
+document.getElementById("areaBlackboard").title = "Новое задание?";
+document.getElementById("areaPupils").title = "Мы - класс! Помочь?";
+document.getElementById("setOp").innerHTML = "Действия";
+document.getElementById("setOpAdd").innerHTML = "сложение";
+document.getElementById("setOpSub").innerHTML = "вычитание";
+document.getElementById("setOpMul").innerHTML = "умножение";
+document.getElementById("setOpDiv").innerHTML = "деление";
+document.getElementById("setOrder").innerHTML = "Порядок";
+document.getElementById("setOrderOnes").innerHTML = "единицы";
+document.getElementById("setOrderTens").innerHTML = "десятки";
+document.getElementById("setOrderTenths").innerHTML = "десятые";
+document.getElementById("setFind").innerHTML = "Найти";
+document.getElementById("setFindRes").innerHTML = "результат";
+document.getElementById("setFindOpd").innerHTML = "операнд";
+document.getElementById("setFindOp").innerHTML = "действие";
+document.getElementById("setFindRel").innerHTML = "отношение";
+document.getElementById("setOpd2nd").innerHTML = "2-й операнд";
+document.getElementById("aboutCharacters").innerHTML = 
+	"Все персонажи вымышлены.<br>Любое сходство<br>с реальными людьми<br>случайно.<br>";
+document.getElementById("helpText").innerHTML = 
+	"Помоги Мише решить примеры!<br><br>" +
+	"Кликни здесь для нового задания.<br>" +
+	"Кликни Марьванну для настройки.<br>" +
+	"Кликни Мишу для просмотра дневника.<br>" +
+	"Кликни класс для справки.";
+document.getElementById("keyMul").innerHTML = "x";
+document.getElementById("keyDiv").innerHTML = ":";
+//
+eExcellent="Отлично, Миша!";
+eGood="Хорошо, Миша.";
+eSatisfy="Попробуй еще раз.";
